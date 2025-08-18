585a697c40b02a1099186244a289a953695f7366
shani-dev01 <https://github.com/Sunnychaudhary01>
2025-08-18 14:56:32 +0530

    fixed

--- js/home.js
+++ js/home.js
@@ -1,6 +1,21 @@
-$(document).ready(function(){
-    
+$(document).ready(function () {
+
     let name = localStorage.getItem('userEmail');
     console.log(name);
     $('#user-name').html(name);
-})
+
+    $(window).on('scroll', function () {
+        let lastScroll = 0;
+        let currentScroll = $(this).scrollTop();
+
+        if (currentScroll > lastScroll) {
+            $('.Navbar').addClass('scroll')
+            console.log('Down', currentScroll)
+        } else {
+            $('.Navbar').removeClass('scroll')
+            console.log('Up:', currentScroll)
+        }
+
+        // console.log(lastScroll)
+    });
+});
