39e00281501ebee9d4f4bdd0e62075a6880bd9f0
shani-dev01 <https://github.com/Sunnychaudhary01>
2025-08-19 20:19:02 +0530

    fixed

--- js/Data.js
+++ js/Data.js
@@ -63,16 +63,18 @@ $(document).ready(function () {
 
 
 
-    // --- logic for fetch data and render to home page ---
+    // --- logic for fetch data and render to slider ---
     const moviesData = async () => {
         let res = await fetch(`${url}/genre/movie/list?api_key=${api_key}`)
         let data = await res.json()
+        // console.log(data.genres[0])
         // await SearchMovies()
         homePagePoster();
     }
+    // --- logic for fetch data and render to slider ---
 
 
-    //  --- logic for search moives --- 
+    //  --- logic for fetch moives for poster --- 
     // const SearchMovies = async() => {
     //     let res = await fetch(`${url}/search/movie?api_key=${api_key}&query=${encodeURIComponent(movieNmae)}`);
     //     let data = await res.json();
@@ -80,16 +82,19 @@ $(document).ready(function () {
     //  }
     //  --- logic for search moives --- 
 
+
+
+
     //  --- logic for movies poster homepage  --- 
     const homePagePoster = async () => {
-        const res = await fetch(`${url}/trending/all/day?api_key=${api_key}`);
-        const data = await res.json();
-        const page = data.results
-        console.log(data.results);
-        const poster = $('.parent-hero-div');
-        page.forEach(function (Element) {
-            console.log(Element)
-            const posterImg = `${img_base_url}${Element.backdrop_path}`
+
+        try {
+            const res = await fetch(`${url}/trending/all/day?api_key=${api_key}`);
+            const data = await res.json();
+            const page = data.results;
+            const poster = $('.parent-hero-div');
+            let genre_id = page[4].id
+            const posterImg = `${img_base_url}${page[4].backdrop_path}`
             $('.parent-hero-div').css({
                 'background-image': `url("${posterImg}")`,
                 'background-size': 'cover',
@@ -97,14 +102,44 @@ $(document).ready(function () {
             });
             const htmlData = `
             <div class="child-hero-div" >
-                <div class="title-div">
-                    <h2>${Element.original_name}</h2>
-                    
                 </div>    
-            </div>
-            `;
+            </div>`;
             poster.append(htmlData);
-        });
+            videoPlay(genre_id, page);
+        } catch (error) {
+            console.log('Error', error);
+        }
+    }
+    //  --- logic for fetch moives for poster ---
+
+    const videoPlay = async (genre_id, page) => {
+        try {
+            const res = await fetch(`${url}/movie/${genre_id}/videos?api_key=${api_key}`)
+            const data = await res.json()
+            const trailer_key = data.results[0].key
+            console.log(trailer_key);
+            const youtubeUrl = `https://www.youtube.com/watch?v=${trailer_key}`
+            console.log(youtubeUrl);
+            const parentDiv = $('.home-page-hero-section')
+            const videoPlay = `
+                <div class="parent-hero-div" >
+                <div class="child-hero-div" >
+                <iframe class="videoLink" src="https://www.youtube.com/embed/${trailer_key}?autoplay=1&controls=0&modestbranding=1&loop=1&playlist=${trailer_key}&mute=1" allowfullscreen></iframe>
+                <div class="title-div">
+                    <h2>${page[4].original_title}</h2>
+                    <span>${page[4].overview}</span>
+                    <div class="btnDivs">
+                    <button class="home-page-play-button" >
+                    <li class="fa-solid fa-play"></li>    Play
+                    </button>
+                    <button class="volumBtn" >volume</button>
+                    </div>
+            </div>
+        </div>`
+            parentDiv.append(videoPlay)
+        } catch (error) {
+            console.error('Error', error);
+        }
     }
 
     moviesData()
