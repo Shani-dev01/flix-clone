16a7e6d5df5dc639d94deaa3acb722a7c2545b8d
shani-dev01 <https://github.com/Sunnychaudhary01>
2025-08-21 00:43:09 +0530

    fixed

--- js/Data.js
+++ js/Data.js
@@ -1,3 +1,4 @@
+let posterImg = '';
 var player;
 var videoIdKey;
 var playerReady = false;
@@ -8,17 +9,18 @@ function onYouTubeIframeAPIReady() {
         'width': '100vw',
         videoId: '',
         playerVars: {
-            autoplay: 0,
-            mute:1,
+            autoplay: 1,
+            mute: 0,
             rel: 0,
             controls: 0,
             modestbranding: 1,
             playsinline: 1,
-            wmode: 'transparent'
+            wmode: 'transparent',
+             fs: 1 
         },
         events: {
             'onReady': onPlayerReady,
-             'onStateChange': onPlayerStateChange
+            'onStateChange': onPlayerStateChange
         }
     })
 }
@@ -28,8 +30,10 @@ function onPlayerReady(event) {
         player.loadVideoById(videoIdKey);
     }
     event.target.playVideo(); // autoplay
+    event.target.mute()
     hidePosterBackground();   // video play hone se bg remove
 }
+
 $(document).on("click", ".volumBtn", function () {
     if (player && player.isMuted()) {
         player.unMute();
@@ -66,7 +70,6 @@ function onPlayerStateChange(event) {
         showPosterBackground(posterImg); // bg wapas
     }
 }
-
 
 $(document).ready(async function () {
 
@@ -123,24 +126,27 @@ $(document).ready(async function () {
     const posterRes = await fetch(`${url}/trending/all/day?api_key=${api_key}`);
     const posterData = await posterRes.json();
     const page = posterData.results;
-    let genre_id = page[3].id
-    console.log(page[3])
+    const movie = page[7];
+    const movieId = movie.id;
+    console.log(page,);
+    
 
 
     //  --- logic for fetch moives for poster ---
 
-    const res = await fetch(`${url}/movie/${genre_id}/videos?api_key=${api_key}`)
+    const res = await fetch(`${url}/movie/${movieId}/videos?api_key=${api_key}`)
     const data = await res.json()
-    const urlKey = data.results[7].key
-    console.log(data.results[0])
+    console.log(data.results)
+    const urlKey = data.results[0].key;
     videoIdKey = urlKey;
-    if (data.results.length > 0) {
+    if (data.results && data.results.length > 0) {
         videoIdKey
         // Sirf tab load karo jab player ready ho
         if (playerReady) {
             player.loadVideoById(videoIdKey);
         } 
-        const posterImg = `${img_base_url}${page[3].backdrop_path}`;
+
+        posterImg = `${img_base_url}${movie.backdrop_path}`;
             $('.parent-hero-div').css({
                     'background-image': `url('${posterImg}')`,
                     'background-size': 'cover',
@@ -148,20 +154,18 @@ $(document).ready(async function () {
                     'background-position': 'center',
                     'position': 'relative',
                     'z-index': '1',
-                });
-
-         
+                }); 
         const parentDiv = $('.parent-hero-div')
         const bannerPoster = `
                 <div class="child-hero-div" >
                 <div class="title-div">
-                    <h2>${page[3].original_title}</h2>
-                    <span>${page[3].overview}</span>
+                    <h2>${movie.original_title}</h2>
+                    <span>${movie.overview}</span>
                     <div class="btnDivs">
                     <button class="home-page-play-button" >
                     <li class="fa-solid fa-play"></li>Play
                     </button>
-                    <button class="volumBtn" >volume</button>
+                    <button class="volumBtn" >unmute</button>
                     </div>
                 </div>    
             </div>`
@@ -170,55 +174,3 @@ $(document).ready(async function () {
     }
     const youtubeUrl = `https://www.youtube.com/watch?v=${urlKey}`;
 });
-
-
-// var player;
-// var videoIdKey;
-// var playerReady = false; // track if player ready hai
-
-// function onYouTubeIframeAPIReady() {
-//     player = new YT.Player('player-container', {
-//         height: '100%',
-//         width: '100%',
-//         videoId: '', // initially blank
-//         playerVars: { autoplay: 0, controls: 0 },
-//         events: {
-//             onReady: function(event) {
-//                 playerReady = true;
-//                 console.log("Player ready!");
-
-//                 // Agar videoIdKey pehle se fetch ho gayi ho
-//                 if(videoIdKey){
-//                     player.loadVideoById(videoIdKey);
-//                 }
-//             }
-//         }
-//     });
-// }
-
-// $(document).ready(async function() {
-//     const api_key = 'abd6102284b0a6e60a5a118ba23efedb';
-//     const url = 'https://api.themoviedb.org/3';
-
-//     async function fetchVideoKey() {
-//         const res = await fetch(`${url}/trending/all/day?api_key=${api_key}`);
-//         const data = await res.json();
-//         const page = data.results;
-//         console.log(page[12])
-//         let genre_id = page[12].genre_ids[1];
-
-//         const videoRes = await fetch(`${url}/movie/${genre_id}/videos?api_key=${api_key}`);
-//         const videoData = await videoRes.json();
-//         if(videoData.results.length > 0){
-//             videoIdKey = videoData.results[0].key;
-//             console.log("videoIdKey set ho gaya:", videoIdKey);
-
-//             // Sirf tab load karo jab player ready ho
-//             if(playerReady){
-//                 player.loadVideoById(videoIdKey);
-//             }
-//         }
-//     }
-
-//     await fetchVideoKey();
-// });
